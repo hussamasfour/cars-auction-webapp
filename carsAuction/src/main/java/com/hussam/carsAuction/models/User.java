07643a04f2c1e21714393f833b18e3f161ea3f50
Hussam Asfour <Hussam_asfour@yahoo.com>
2022-09-12 11:26:54 -0400

    added user repository and switch user class to use lombok for getter and setter

--- carsAuction/src/main/java/com/hussam/carsAuction/models/User.java
+++ carsAuction/src/main/java/com/hussam/carsAuction/models/User.java
@@ -1,9 +1,18 @@
 package com.hussam.carsAuction.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
+/**
+ * Pojo class for user entity
+ */
+@Getter
+@Setter
 @Entity
 @Table(name = "user")
 public class User {
@@ -27,53 +36,6 @@ public class User {
     @Column(nullable = false, length = 10)
     private Long phone;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Long getPhone() {
-        return phone;
-    }
-
-    public void setPhone(Long phone) {
-        this.phone = phone;
-    }
 
     @Override
     public String toString() {
---
+++ carsAuction/src/main/java/com/hussam/carsAuction/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.hussam.carsAuction.repositories;
+
+import com.hussam.carsAuction.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+}

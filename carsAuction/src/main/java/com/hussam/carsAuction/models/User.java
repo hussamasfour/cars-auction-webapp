9aefc699f85f2359cc0e8f6974a80bb082ba94b4
Hussam Asfour <Hussam_asfour@yahoo.com>
2022-09-12 11:19:02 -0400

    created a user entity and added constraints validation for the fields

---
+++ carsAuction/src/main/java/com/hussam/carsAuction/models/User.java
@@ -0,0 +1,88 @@
+package com.hussam.carsAuction.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @NotNull
+    @Column(name = "firstName", nullable = false, length = 30)
+    private String firstName;
+    @NotNull
+    @Column(name = "lastName", nullable = false, length = 30)
+    private String lastName;
+    @Email
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name="password", nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 10)
+    private Long phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", phone=" + phone +
+                '}';
+    }
+}

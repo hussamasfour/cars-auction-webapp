e77991fd3519382f4ee9c6a89bbe91f15ef65de5
Hussam Asfour <Hussam_asfour@yahoo.com>
2022-09-12 09:45:15 -0400

    initial commit

---
+++ carsAuction/src/main/java/com/hussam/carsAuction/CarsAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.hussam.carsAuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsAuctionApplication.class, args);
+	}
+
+}
---
+++ carsAuction/src/test/java/com/hussam/carsAuction/CarsAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hussam.carsAuction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

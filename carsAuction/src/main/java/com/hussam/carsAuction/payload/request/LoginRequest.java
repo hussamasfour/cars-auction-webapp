32c39a3bdc0d9041e0893b64576d3bd1fa9588ad
Hussam Asfour <Hussam_asfour@yahoo.com>
2022-10-04 11:11:21 -0400

    added validation for payload classes

--- carsAuction/src/main/java/com/hussam/carsAuction/payload/request/LoginRequest.java
+++ carsAuction/src/main/java/com/hussam/carsAuction/payload/request/LoginRequest.java
@@ -2,10 +2,15 @@ package com.hussam.carsAuction.payload.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class LoginRequest {
-
+    @NotBlank(message = "Please enter an email")
+    @Email
     private String email;
-
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 }
--- carsAuction/src/main/java/com/hussam/carsAuction/payload/request/SignUpRequest.java
+++ carsAuction/src/main/java/com/hussam/carsAuction/payload/request/SignUpRequest.java
@@ -8,8 +8,13 @@ import javax.validation.constraints.Size;
 
 @Data
 public class SignUpRequest {
+    @NotBlank(message = "FirstName is required")
     private String firstName;
+    @NotBlank(message = "LastName is required")
     private String lastName;
+    @NotBlank(message = "Email is required")
+    @Email
     private String email;
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 }
